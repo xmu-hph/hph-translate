5d8a91b6b3cd72ea16074a2ed4db332a4368e3d1
xmu-hph <155121366+xmu-hph@users.noreply.github.com>
2024-05-04 14:37:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    url.hostname = "translate.google.com";
+    url.hostname = "translate.googleapis.com";
     return fetch(new Request(url, request));
   },
 };
